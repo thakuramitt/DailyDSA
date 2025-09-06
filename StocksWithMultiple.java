d1e3cbe6ac647e1abbf35efe48d90719eb820799
Amit Singh <tamitsingh.io@gmail.com>
2025-09-06 22:18:53 +0530

    Added optimized approach for BuyandSell Stocks

--- StocksWithMultiple.java
+++ StocksWithMultiple.java
@@ -3,28 +3,48 @@
 
 // Brute Force - Exponential Time Complexity
 public class StocksWithMultiple {
-    public static int getProfit(int[] prices, int start, int end) {
-
+    public static int getProfit(int[] prices) {
         int res = 0;
+        int i = 0;
+        int n = prices.length;
+        int localMinima = prices[0];
+        int localMaxima = prices[0];
 
-        for(int i = start; i < end; i++) {
-            for(int j = i+1; j <= end; j++) {
-                if(prices[j] > prices[i]) {
-                    int curr = prices[j] - prices[i] + getProfit(prices, start, i-1) + getProfit(prices, j+1, end);
-                    res = Math.max(res, curr);
-                }
+        while(i < n-1) {
 
-            }
+            while(i < n-1 && prices[i] >= prices[i+1]) i++;
+            localMinima = prices[i];
+
+            while(i < n-1 && prices[i] <= prices[i+1]) i++;
+            localMaxima = prices[i];
+
+            res += localMaxima - localMinima;
+
         }
-
         return res;
-
     }
+//    public static int getProfit(int[] prices, int start, int end) {
+//
+//        int res = 0;
+//
+//        for(int i = start; i < end; i++) {
+//            for(int j = i+1; j <= end; j++) {
+//                if(prices[j] > prices[i]) {
+//                    int curr = prices[j] - prices[i] + getProfit(prices, start, i-1) + getProfit(prices, j+1, end);
+//                    res = Math.max(res, curr);
+//                }
+//
+//            }
+//        }
+//
+//        return res;
+//
+//    }
     public static void main(String[] args) {
 
         int[] prices = {100, 180, 260, 310, 40, 535, 695};
 
-        int ans = getProfit(prices, 0, prices.length-1);
+        int ans = getProfit(prices);
 
         System.out.println(ans);
     }
