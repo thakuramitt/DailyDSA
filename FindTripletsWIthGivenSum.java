052f94c3d47732dcb14fc066e8835c6505322fb9
Amit Singh <tamitsingh.io@gmail.com>
2025-09-07 22:48:25 +0530

    Added GFG problem 3Sum with given sum

---
+++ FindTripletsWIthGivenSum.java
@@ -0,0 +1,44 @@
+//https://www.geeksforgeeks.org/dsa/print-all-triplets-with-given-sum/
+
+import java.util.Arrays;
+
+public class FindTripletsWIthGivenSum {
+    public static int solve(int[] prices, int target) {
+        int currSum = 0;
+
+        Arrays.sort(prices);
+
+        for(int left = 0; left < prices.length - 2; left++) {
+
+            if(left > 0 && prices[left] == prices[left - 1]) continue;
+            int right = left + 1;
+            int last = prices.length -1;
+
+            while(right < last) {
+                int curr = prices[left] + prices[right] + prices[last];
+                if(curr == target) {
+                    currSum++;
+                    right++;
+                    last--;
+                }
+                else if (curr < target) right++;
+                else last--;
+
+                while(right < last && prices[right] == prices[right-1]) right++;
+                while(right < last && prices[last] == prices[last+1]) last--;
+            }
+        }
+
+        return currSum;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, -2, 1, 0, 5};
+        int target = 1;
+
+        System.out.println(solve(arr, target));
+
+    }
+}
