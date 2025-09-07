bd34307b47134db97ae007a8eb3060bdcd94202f
Amit Singh <tamitsingh.io@gmail.com>
2025-09-07 22:30:53 +0530

    Added Leetcode 122 Buy Stocks II

--- BestTimeToBuyStocks2.java
+++ BestTimeToBuyStocks2.java
@@ -1,6 +1,7 @@
 //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
 
 public class BestTimeToBuyStocks2 {
+//     Without using DP
     public static int solve(int[] arr) {
         int currBuy = arr[0];
         int profit = 0;
@@ -15,11 +16,33 @@ public class BestTimeToBuyStocks2 {
         }
         return profit;
     }
+
+//     Using Dynamic Programming
+
+    public static int solve(int[] prices, Integer[][] dp, int idx, int canBuy) {
+
+        if(idx == prices.length) return 0;
+
+        if(dp[idx][canBuy] != null) return dp[idx][canBuy];
+
+        int skip = solve(prices, dp, idx+1, canBuy);
+
+        if(canBuy == 1) {
+            int buy = -prices[idx] + solve(prices, dp, idx+1, 0);
+            return dp[idx][canBuy] = Math.max(buy, skip);
+        } else {
+            int sell = prices[idx] + solve(prices, dp, idx+1, 1);
+            return dp[idx][canBuy] = Math.max(sell, skip);
+        }
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {7,6,4,3,1};
+        int n = arr.length;
+        Integer[][] dp = new Integer[n][2];
 
-        System.out.println(solve(arr));
+        System.out.println(solve(arr, dp, 0, 1));
 
     }
 }
